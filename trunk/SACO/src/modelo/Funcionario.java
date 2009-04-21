2d8861db34b6c6d559b8434efe10e8e2f3dd0029
wavegirlbr <wavegirlbr@681ae5e6-2d5d-11de-958b-d3ca58845339>
2009-04-21 14:05:20 +0000

    Falta passar do teste US1, a partir do 35, o problema é com o login, o tratamento tinha sido feito até agora em funcionario, mas o login não pode ser repetido então tem q fazer uma pesquisa no sistema p/ achar se o novo login já existe, então o tratamento do login tem q ser feito na classe sistema.Temos uma lista de usuários, que tá incluindo tanto usuários quanto clientes.Tô na dúvida c/ relação a busca pelo login nessa lista.Buscamos nessa lista mesmo, ou separamos uma lista p/ funcionarios e outra p/ clientes??

--- trunk/SACO/src/modelo/Funcionario.java
+++ trunk/SACO/src/modelo/Funcionario.java
@@ -1,5 +1,11 @@
 package modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.sun.xml.internal.bind.v2.runtime.Name;
+
+import excecoes.CampoInvalidoException;
 import excecoes.CampoObrigatorioException;
 
 public class Funcionario implements Pessoa {
@@ -8,20 +14,100 @@ public class Funcionario implements Pessoa {
 	private String nome;
 	private String email;
 	private String telefone;
+	private int NUM_DIGITOS_FONE = 10;
 
-	public Funcionario(String login, String nome, String email, String telefone) throws CampoObrigatorioException {
-		if(login == null || login.equals("")){
+	public Funcionario(String login, String nome, String email, String telefone) throws CampoObrigatorioException, CampoInvalidoException {
+		
+		//tratando argumentos nulos
+		
+		if (login == null && nome == null && email == null && telefone == null){
+			throw new CampoObrigatorioException("error: all fields are mandatory!");
+		}
+		
+		if (login == null){
 			throw new CampoObrigatorioException("error: login is a mandatory field!");
 		}
-		if(nome == null || nome.equals("")){
+		
+		
+		if(nome == null){
 			throw new CampoObrigatorioException("error: name is a mandatory field!");
 		}
-		if(email == null || email.equals("")){
+		
+		if(email == null){
 			throw new CampoObrigatorioException("error: e-mail is a mandatory field!");
 		}
-		if(telefone == null || telefone.equals("")){
+		
+		if(telefone == null){
 			throw new CampoObrigatorioException("error: phone number is a mandatory field!");
 		}
+		
+		//tratando argumentos vazios
+		
+		if (login.equals("") & nome.equals("") & email.equals("") & telefone.equals("")){
+			throw new CampoObrigatorioException("error: all fields are mandatory!"); 
+		}
+		
+		if(login.equals("")){
+			throw new CampoObrigatorioException("error: login is a mandatory field!");
+		}
+		if(nome.equals("")){
+			throw new CampoObrigatorioException("error: name is a mandatory field!");
+		}
+		if(email.equals("")){
+			throw new CampoObrigatorioException("error: e-mail is a mandatory field!");
+		}
+		if(telefone.equals("")){
+			throw new CampoObrigatorioException("error: phone number is a mandatory field!");
+		}
+		
+		//tratando login invalido
+		
+		if ( login.charAt(0)== '-' || login.charAt(0)== '_' || login.charAt(0)== '.'){
+			throw new CampoInvalidoException("error: invalid field!");
+		}
+		
+		if ( login.charAt(login.length()-1) == '-' || login.charAt(login.length()-1) == '_' || login.charAt(login.length()-1) == '.'){
+			throw new CampoInvalidoException("error: invalid field!");
+		}
+		
+		if (login.contains("@")){
+			throw new CampoInvalidoException("error: invalid field!");
+		}
+		
+		//tratando nome invalido
+		
+		if ( nome.charAt(0)== '-' || nome.charAt(0)== '_' || nome.charAt(0)== '.'){
+			throw new CampoInvalidoException("error: invalid field!");
+		}
+		
+		if ( nome.charAt(nome.length()-1) == '-' || nome.charAt(nome.length()-1) == '_' || nome.charAt(nome.length()-1) == '.'){
+			throw new CampoInvalidoException("error: invalid field!");
+		}
+		
+		if (nome.contains("@")){
+			throw new CampoInvalidoException("error: invalid field!");
+		}
+		
+		//tratando email invalido
+		
+		if ( email.charAt(0)== '-' || email.charAt(0)== '_' || email.charAt(0)== '.'){
+			throw new CampoInvalidoException("error: invalid field!");
+		}
+		
+		if ( email.charAt(email.length()-1) == '-' || email.charAt(email.length()-1) == '_' || email.charAt(email.length()-1) == '.'){
+			throw new CampoInvalidoException("error: invalid field!");
+		}
+		
+		//tratando telefone invalido
+		
+		
+		if (!telefone.matches("[0-9]{10}")){
+			throw new CampoInvalidoException("error: invalid field!");
+		}
+		
+		
+		
+		this.login = login;		
 		this.nome = nome;
 		this.email = email;
 		this.telefone = telefone;
